90bc6510878724b45b5d073bd73e3f3818f71543
Mathys Cogné <mathys.cognef@gmail.com>
2023-07-19 20:59:24 +0200

    Init react with vite

--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
-const multer = require('multer');
+
 
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
